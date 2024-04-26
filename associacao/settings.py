c3f011c0b434490a92b2c684cc053e57be1a4c7b
Ricardo Sousa <r.sousa.888@gmail.com>
2024-04-26 11:37:29 +0100

    corrigindo erros

--- associacao/settings.py
+++ associacao/settings.py
@@ -109,10 +109,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "/media/"
 # Default primary key field type
