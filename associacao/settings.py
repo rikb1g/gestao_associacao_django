a2f1029fadaa6e6dac9cff9db4a68849a94ad051
Ricardo Sousa <r.sousa.888@gmail.com>
2024-04-26 15:13:59 +0100

    correçao

--- associacao/settings.py
+++ associacao/settings.py
@@ -67,14 +67,14 @@ WSGI_APPLICATION = 'associacao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
